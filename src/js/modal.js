8f5baee69d3d504d2d6b3846967b489ea3aabfc3
Dark-ManX <sergiosis@ukr.net>
2022-06-12 13:52:11 +0300

    End with markup and styles. Begin js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,13 @@
 // функционал открытия-закрытия модального окна, используется дя карточки фильма и в футере
+
+const refs = {
+  closeBtn: document.querySelector('.modal__close'),
+  modal: document.querySelector('.modal'),
+};
+console.log(refs.closeBtn);
+
+refs.closeBtn.addEventListener('click', onCloseBtnCLick);
+
+function onCloseBtnCLick(e) {
+  return refs.modal.classList.toggle('modal__shown');
+}
