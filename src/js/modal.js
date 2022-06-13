0dd850506ca221b3881ef96cdef1ad79d9414019
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2022-06-13 08:38:17 +0300

    base modal to pull

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,32 @@
 // функционал открытия-закрытия модального окна, используется дя карточки фильма и в футере
+import MicroModal from 'micromodal';
+import '../sass/base/_modal.scss';
+
+export const openModal = id => {
+  MicroModal.show(id);
+};
+
+// ***** in js file example
+
+// import { openModal } from './modal';
+
+// const element = document.querySelector('SELECTOR');
+
+// element.addEventListener('click', () => {
+//   openModal('ID_OF_MODAL');
+// });
+
+// ***** base markup of modal
+
+// <div class="ENY__CLASS__YOU__NEED micromodal-slide" aria-hidden="true" id="ID_OF_MODAL">
+//   <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+//     <div>
+//       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+//          -------- START OF BODY
+//         <button class="" aria-label="Close modal" data-micromodal-close>
+//         </button>
+//          -------- END OF BODY
+//       </div>
+//     </div>
+//   </div>
+// </div>;
