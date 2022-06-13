3eeb683751e5a050a940e54d88cd9d8b40decb80
Iryna Holova <gis.golova@gmail.com>
2022-06-13 04:10:59 +0300

    add spinner

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,1 +1,10 @@
 // функционал спиннера, используется в асинхронных функциях
+
+const markup = `<div class="half-circle-spinner">
+  <div class="circle circle-1"></div>
+  <div class="circle circle-2"></div>
+</div>`;
+
+export default function addSpinner(container) {
+    container.innerHTML = markup;
+}
