efd417778d4942f16df17bced7ed453a64ece6f6
Dark-ManX <sergiosis@ukr.net>
2022-06-13 17:34:08 +0300

    need add

--- src/js/modal-gallery.js
+++ src/js/modal-gallery.js
@@ -1,1 +1,11 @@
 // функционал отправки карточки в хранилище по клику на кнопки
+
+const modalGalleryRefs = {
+  addToWatchedBtn: document.querySelector('.modal-button'),
+  addToQueueBtn: document.querySelector('.modal-button__queue'),
+};
+
+addToWatchedBtn.addEventListener('click', onAddWatched);
+addToQueueBtn.addEventListener('click', onAddQueue);
+
+// function onAddWatched(e) {}
