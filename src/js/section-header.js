dea3f3041e64a222f4139fe2664cfd0e82c3ec38
Igor Ostapenko <igorostap9@gmail.com>
2022-06-13 22:11:49 +0300

    header-js

--- src/js/section-header.js
+++ src/js/section-header.js
@@ -1,1 +1,26 @@
 //основной скрипт хедера, импортирован в index.js
+const refs = {
+    headerBgc: document.querySelector('.header'),
+    myLibrary : document.querySelector('[data-page="library"]'),
+    homePage : document.querySelector('.header-nav__link'),
+    form : document.querySelector('.header-search'),
+    libButton: document.querySelector(".headers-button"),
+    searchForm: document.querySelector('.header-search__input'),
+    gallery: document.querySelector('.film-list'),
+}
+
+refs.myLibrary.addEventListener('click',myLibraryFilm)
+function myLibraryFilm(e) {
+    e.preventDefault();
+    refs.homePage.classList.remove('header-nav--current');
+    refs.myLibrary.classList.add('header-nav--current');
+    refs.headerBgc.classList.remove('home');
+    refs.headerBgc.classList.add('library');
+    clearMovieList();
+    refs.form.style.display = "none";
+    refs.libButton.style.display = "flex";
+
+}
+function clearMovieList() {
+       refs.gallery.innerHTML = '';
+    };
