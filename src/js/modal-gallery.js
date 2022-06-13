d8feb9ca83fc8999c68957eea004232bfa10bb66
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2022-06-13 23:06:34 +0300

    final commit from max

--- src/js/modal-gallery.js
+++ src/js/modal-gallery.js
@@ -1,11 +1,1 @@
 // функционал отправки карточки в хранилище по клику на кнопки
-
-const modalGalleryRefs = {
-  addToWatchedBtn: document.querySelector('.modal-button'),
-  addToQueueBtn: document.querySelector('.modal-button__queue'),
-};
-
-addToWatchedBtn.addEventListener('click', onAddWatched);
-addToQueueBtn.addEventListener('click', onAddQueue);
-
-// function onAddWatched(e) {}
