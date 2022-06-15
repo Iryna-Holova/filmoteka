07d96cbe7642a0753ddf3bcb81c53c54196dd7ed
Igor Ostapenko <igorostap9@gmail.com>
2022-06-15 21:36:36 +0300

    header button/page home/library

--- src/js/section-header.js
+++ src/js/section-header.js
@@ -1,25 +1,55 @@
 //основной скрипт хедера, импортирован в index.js
+import Gallery from "./section-gallery";
+const Galleri = new Gallery();
+
 const refs = {
     headerBgc: document.querySelector('.header'),
     myLibrary : document.querySelector('[data-page="library"]'),
-    homePage : document.querySelector('.header-nav__link'),
+    homePage : document.querySelector('[data-page="home"]'),
     form : document.querySelector('.header-search'),
     libButton: document.querySelector(".headers-button"),
     searchForm: document.querySelector('.header-search__input'),
     gallery: document.querySelector('.film-list'),
+    buttonWatch: document.querySelector('.header-button__watch'),
+    buttonQueue: document.querySelector('.header-button__que'),
 }
+refs.myLibrary.addEventListener('click', myLibraryFilm);
+refs.homePage.addEventListener('click', homePageFilm);
+refs.buttonWatch.addEventListener('click', () => {
+    refs.buttonQueue.style.backgroundColor = "";
+    refs.buttonWatch.style.backgroundColor = "#FF6B01";
+    Galleri.showMylibraryWatch();
+});
+refs.buttonQueue.addEventListener('click', () => {
+    refs.buttonQueue.style.backgroundColor = "#FF6B01";
+    refs.buttonWatch.style.backgroundColor = "";
+    Galleri.showMylibraryQueue();
+})
 
-refs.myLibrary.addEventListener('click',myLibraryFilm)
 function myLibraryFilm(e) {
     e.preventDefault();
+    clearMovieList();
     refs.homePage.classList.remove('header-nav--current');
     refs.myLibrary.classList.add('header-nav--current');
     refs.headerBgc.classList.remove('home');
     refs.headerBgc.classList.add('library');
-    clearMovieList();
     refs.form.style.display = "none";
     refs.libButton.style.display = "flex";
+    refs.buttonQueue.style.backgroundColor = "#FF6B01";
+    refs.buttonWatch.style.backgroundColor = "";
+    Galleri.showMylibraryWatch();
 
+}
+function homePageFilm(e){
+    e.preventDefault();
+    clearMovieList();
+    refs.homePage.classList.add('header-nav--current');
+    refs.myLibrary.classList.remove('header-nav--current');
+    refs.headerBgc.classList.add('home');
+    refs.headerBgc.classList.remove('library');
+    refs.form.style.display = "flex";
+    refs.libButton.style.display = "none";
+    Galleri.showHome();
 }
 function clearMovieList() {
        refs.gallery.innerHTML = '';
