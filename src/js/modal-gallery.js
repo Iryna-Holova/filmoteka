b13f173ac3df25453d1f49e015f942ee0a22e09c
Dark-ManX <sergiosis@ukr.net>
2022-06-16 23:46:48 +0300

    Remove scroll

--- src/js/modal-gallery.js
+++ src/js/modal-gallery.js
@@ -1,14 +1,16 @@
 // функционал отправки карточки в хранилище по клику на кнопки
+import FetchMoviesApiService from './fetch_movies_api';
+
 const modalRefs = {
   modalBtnListener: document.querySelector('.modal-gallery'),
 };
 
-const obj = {};
 modalRefs.modalBtnListener.addEventListener('click', onModalBtnClick);
 
 function onModalBtnClick(e) {
   if (e.target.className === 'button modal-gallery-button') {
-    localStorage.setItem('watched', JSON.stringify(obj));
+    console.log(FetchMoviesApiService.fetchMovieByID(movieID));
+    localStorage.setItem('watched', JSON.stringify());
   } else if (e.target.className === 'modal-gallery-button__queue') {
     localStorage.setItem('queue', JSON.stringify(obj));
   }
